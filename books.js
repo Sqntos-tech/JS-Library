671f7234d305e7e819ac6ded25df3005aa53403d
Sqntos-tech <edwardsantos1237@gmail.com>
2026-08-27 20:30:11 -0400

    update

--- books.js
+++ books.js
@@ -1,13 +1,21 @@
+ let books;
+ 
  async function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books")
-  
-  const books = await getBooks()
+
+  booksWrapper.classList += ' books__loading'
+
+  if (!books) {
+    books = await getBooks()
+  }
+
+  booksWrapper.classList.remove('books__loading')
 
   if (filter === 'LOW_TO_HIGH') {
-   books.sort((a,b) => a.originalPrice - b.originalPrice) 
+   books.sort((a,b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)) 
   }
   else if (filter === 'HIGH_TO_LOW') {
-   books.sort((a,b) => b.originalPrice - a.originalPrice) 
+   books.sort((a,b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)) 
   }
   else if (filter === 'RATING') {
    books.sort((a,b) => b.rating - a.rating) 
@@ -27,7 +35,7 @@
      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span>$${book.originalPrice.toFixed(2)}</span>
+      ${priceHTML(book.originalPrice, book.salePrice)}
     </div>
   </div>`
   })
@@ -35,6 +43,13 @@
   .join("")
 
   booksWrapper.innerHTML = booksHtml
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span>$${salePrice.toFixed(2)}`
 }
 
  function ratingsHTML(rating) {
